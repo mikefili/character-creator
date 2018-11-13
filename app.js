da1f6fb12f004a25b1a0cfdf103bc8b6c170765c
Regan Dufort <regan.t.dufort@gmail.com>
2018-11-13 13:48:53 -0800

    Third button working with total random button working

--- app.js
+++ app.js
@@ -1,81 +1,63 @@
+'use strict';
 
-
-
-
-var racearray=['ta','tb','tx','td','te','tf','tyyy','trrrr','grw4gw'];
+var racearray = ['ta', 'tb', 'tx', 'td', 'te', 'tf', 'tyyy', 'trrrr', 'grw4gw'];
 //chose random race
 
-document.getElementById('randomrace').addEventListener('click',generaterandomrace);
+document.getElementById('randomrace').addEventListener('click', generaterandomrace);
+function generaterandomrace() {
 
-function generaterandomrace(){
-
-  var randomindex=Math.floor(Math.random()*9);
-
-
-  document.getElementById('b').value=racearray[randomindex];
-
-  document.getElementById('a').value=randomindex;
+  var randomindex = Math.floor(Math.random() * 9);
+  document.getElementById('b').value = racearray[randomindex];
+  document.getElementById('a').value = randomindex;
 }
 
-//chose race
+//choose race
 
-function showrace(){
+function showrace() {
 
-  var selectedrace=document.getElementById('a').value;
-  document.getElementById('b').value=racearray[selectedrace];
-
-
+  var selectedrace = document.getElementById('a').value;
+  document.getElementById('b').value = racearray[selectedrace];
 }
 showrace();
 
+// choose random class
 
+var classarray = ['1', 'a', '3', '5', 'ff', 'ffsf', 'er', 'g', 'ty', 'jh', 'j', 'rhfn'];
+document.getElementById('randomclass').addEventListener('click', generaterandomclass);
 
+function generaterandomclass() {
 
-
-// chose random class
-
-var classarray=['1','a','3','5','ff','ffsf','er','g','ty','jh','j','rhfn'];
-document.getElementById('randomclass').addEventListener('click',generaterandomclass);
-
-function generaterandomclass(){
-
-  var randomindex=Math.floor(Math.random()*12);
-  document.getElementById('d').value=classarray[randomindex];
-  document.getElementById('c').value=randomindex;
-
+  var randomindex = Math.floor(Math.random() * 12);
+  document.getElementById('d').value = classarray[randomindex];
+  document.getElementById('c').value = randomindex;
 }
 
-// chose class
+// choose class
 
+function showclass() {
 
-function showclass(){
-
-  var selectedrace=document.getElementById('c').value;
-  document.getElementById('d').value=racearray[selectedrace];
-
-
+  var selectedrace = document.getElementById('c').value;
+  document.getElementById('d').value = racearray[selectedrace];
 }
 showclass();
 
-
-
 // Generate random class for third button randomize
 
-var thirdRandomClass = ['description', 'description','description', 'description', 'description', 'description', 'description','description','description','description','description','description','description'];
+var thirdRandomClass = ['description', 'description', 'description', 'description', 'description', 'description', 'description', 'description', 'description', 'description', 'description', 'description', 'description'];
 document.getElementById('randombackground').addEventListener('click', generateThirdRandomClass);
 
-function generateThirdRandomClass(){
+function generateThirdRandomClass() {
 
-var randomindex = Math.floor(Math.random() * 13);
-document.getElementById('third').value=thirdRandomClass[randomindex];
-document.getElementById('generatethirdclass').value=randomindex;
+  var randomindex = Math.floor(Math.random() * 13);
+  document.getElementById('third').value = thirdRandomClass[randomindex];
+  document.getElementById('generatethirdclass').value = randomindex;
 
 }
 
-function showThirdClass(){
+function showThirdClass() {
 
   var randomThird = document.getElementById('generatethirdclass').value;
-  document.getElementById('thirdclass').value=racearray[randomThird];
+  document.getElementById('third').value = racearray[randomThird];
 
 }
 
@@ -85,23 +67,38 @@ showThirdClass();
 
 
 
+
+document.getElementById('randomize-all').addEventListener('click', generateAllclasses);
+function generateAllclasses(event) {
+  
+event.preventDefault();
+
+  generaterandomrace();
+  generaterandomclass();
+  generateThirdRandomClass();
+  generaterandomalignment();
+}
+
+
+
+
 //pick one button from 9 options;
 
-var n=0;
-var buttonel=document.querySelectorAll('.alel');
-for(var i=0;i<buttonel.length;i++){
-  buttonel[i].index=i;
-  buttonel[i].setAttribute('isAct','0');
-  buttonel[i].onclick=function(){
-    var isAct=this.getAttribute('isAct');
-    if(Number(isAct)){
-      this.setAttribute('isAct','0');
+var n = 0;
+var buttonel = document.querySelectorAll('.alel');
+for (var i = 0; i < buttonel.length; i++) {
+  buttonel[i].index = i;
+  buttonel[i].setAttribute('isAct', '0');
+  buttonel[i].onclick = function () {
+    var isAct = this.getAttribute('isAct');
+    if (Number(isAct)) {
+      this.setAttribute('isAct', '0');
     }
-    else{
+    else {
 
-      buttonel[n].setAttribute('isAct','0');
-      this.setAttribute('isAct','1');
-      n=this.index;
+      buttonel[n].setAttribute('isAct', '0');
+      this.setAttribute('isAct', '1');
+      n = this.index;
     }
   };
 
@@ -110,17 +107,17 @@ for(var i=0;i<buttonel.length;i++){
 }
 
 //randomly pick one button
-document.getElementById('randomalignment').addEventListener('click',generaterandomalignment);
+document.getElementById('randomalignment').addEventListener('click', generaterandomalignment);
 
-function generaterandomalignment(){
+function generaterandomalignment() {
 
-  var buttonel=document.querySelectorAll('.alel');
+  var buttonel = document.querySelectorAll('.alel');
 
-  var randomindexnumber=Math.floor(Math.random()*9);
-  for(var i=0;i<buttonel.length;i++){
+  var randomindexnumber = Math.floor(Math.random() * 9);
+  for (var i = 0; i < buttonel.length; i++) {
 
-    buttonel[i].setAttribute('isAct','0');
+    buttonel[i].setAttribute('isAct', '0');
   }
 
-  buttonel[randomindexnumber].setAttribute('isAct','1');
+  buttonel[randomindexnumber].setAttribute('isAct', '1');
 }
