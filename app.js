fc66de0f2bc8cf16b902348baffaaca2c41cb8e5
Regan Dufort <regan.t.dufort@gmail.com>
2018-11-13 20:39:07 -0800

    generate random background and generate random for all

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 // var result=[];
-var raceoptions=[];
-var classoptions=[];
-var backgroundoptions=[];
-var alignmentoptions=[];
+var raceoptions = [];
+var classoptions = [];
+var backgroundoptions = [];
+var alignmentoptions = [];
 
-var racearray=[
+var racearray = [
   'Description: Kingdoms rich in ancient grandeur, halls carved into the roots of mountains, the echoing of picks and hammers in deep mines and blazing forges, a commitment to clan and tradition, and a burning hatred of goblins and orcs—these common threads unite all dwarves.',
   'Description: Elves are a magical people of otherworldly grace, living in the world but not entirely part of it. They live in places of ethereal beauty, in the midst of ancient forests or in silvery spires glittering with faerie light, where soft music drifts through the air and gentle fragrances waft on the breeze. Elves love nature and magic, art and artistry, music and poetry, and the good things of the world.',
   'Description: The comforts of home are the goals of most halflings’ lives: a place to settle in peace and quiet, far from marauding monsters and clashing armies; a blazing fire and a generous meal; fine drink and fine conversation. Though some halflings live out their days in remote agricultural communities, others form nomadic bands that travel constantly, lured by the open road and the wide horizon to discover the wonders of new lands and peoples. But even these wanderers love peace, food, hearth, and home, though home might be a wagon jostling along a dirt road or a raft floating downriver.',
@@ -17,26 +17,23 @@ var racearray=[
   'Description: To be greeted with stares and whispers, to suffer violence and insult on the street, to see mistrust and fear in every eye: this is the lot of the tiefling. And to twist the knife, tieflings know that this is because a pact struck generations ago infused the essence of Asmodeus—overlord of the Nine Hells—into their bloodline. Their appearance and their nature are not their fault but the result of an ancient sin, for which they and their children and their children’s children will always be held accountable.'];
 //chose random race
 
-document.getElementById('randomrace').addEventListener('click',generaterandomrace);
+document.getElementById('randomrace').addEventListener('click', generaterandomrace);
 
-function generaterandomrace(e){
- e.preventDefault();
-  var randomindex=Math.floor(Math.random()*9);
+function generaterandomrace() {
+  var randomindex = Math.floor(Math.random() * 9);
 
 
-  document.getElementById('b').value=racearray[randomindex];
-
-  document.getElementById('a').value=randomindex;
-
+  document.getElementById('b').value = racearray[randomindex];
+  document.getElementById('a').value = randomindex;
   raceoptions.push(document.getElementById('a').options[document.getElementById('a').selectedIndex].text);
 }
 
 //chose race
 // var racepick;
-function showrace(){
+function showrace() {
 
-  var selectedrace=document.getElementById('a').value;
-  document.getElementById('b').value=racearray[selectedrace];
+  var selectedrace = document.getElementById('a').value;
+  document.getElementById('b').value = racearray[selectedrace];
   // return racepick=racearray[selectedrace];
   raceoptions.push(document.getElementById('a').options[document.getElementById('a').selectedIndex].text);
 }
@@ -44,11 +41,9 @@ function showrace(){
 showrace();
 
 
-
-
 //chose random class
 
-var classarray=[
+var classarray = [
   'Description: A fierce warrior of primitive background who can enter a battle rage',
   'Description: An inspiring magician whose power echoes the music of creation',
   'Description: A priestly champion who wields divine magic in service of a higher power',
@@ -63,62 +58,112 @@ var classarray=[
   'Description: A scholarly magic-user capable of manipulating the structures of reality'
 
 ];
-document.getElementById('randomclass').addEventListener('click',generaterandomclass);
+document.getElementById('randomclass').addEventListener('click', generaterandomclass);
 
-function generaterandomclass(){
+function generaterandomclass() {
 
-  var randomindex=Math.floor(Math.random()*12);
-  document.getElementById('d').value=classarray[randomindex];
-  document.getElementById('c').value=randomindex;
+  var randomindex = Math.floor(Math.random() * 12);
+  document.getElementById('d').value = classarray[randomindex];
+  document.getElementById('c').value = randomindex;
 
   classoptions.push(document.getElementById('c').options[document.getElementById('c').selectedIndex].text);
 }
 
 //chose class
 
-function showclass(){
+function showclass() {
 
-  var selectedclass=document.getElementById('c').value;
-  document.getElementById('d').value=classarray[selectedclass];
+  var selectedclass = document.getElementById('c').value;
+  document.getElementById('d').value = classarray[selectedclass];
 
   classoptions.push(document.getElementById('c').options[document.getElementById('c').selectedIndex].text);
 
 }
 showclass();
 
+//Generate random background
+
+var backgroundarray = [
+  'Description: A fierce warrior of primitive background who can enter a battle rage',
+  'Description: An inspiring magician whose power echoes the music of creation',
+  'Description: A priestly champion who wields divine magic in service of a higher power',
+  'Description: A priest of the Old Faith, wielding the powers of nature - moonlight and plant growth, fire and lightning – and adopting animal forms',
+  'Description: A master of martial combat, skilled with a variety of weapons and armor',
+  'Description: A master of martial arts, harnessing the power of the body in pursuit of physical and spiritual perfection',
+  'Description: A holy warrior bound to a sacred oath',
+  'Description: A warrior who uses martial prowess and nature magic to combat threats on the edges of civilization',
+  'Description: A scoundrel who uses stealth and trickery to overcome obstacles and enemies',
+  'Description: A spellcaster who draws on inherent magic from a gift or bloodline',
+  'Description: A wielder of magic that is derived from a bargain with an extraplanar entity',
+  'Description: A scholarly magic-user capable of manipulating the structures of reality'
+
+];
 
 
+document.getElementById('randombackground').addEventListener('click', generaterandombackground);
+
+function generaterandombackground() {
+
+  var randomindex = Math.floor(Math.random() * backgroundarray.length);
+
+  document.getElementById('f').value = backgroundarray[randomindex];
+  document.getElementById('e').value = randomindex;
+
+  backgroundoptions.push(document.getElementById('e').options[document.getElementById('e').selectedIndex].text);
+
+}
+
+function showBackground() {
+
+  var selectedclass = document.getElementById('c').value;
+  document.getElementById('d').value = classarray[selectedclass];
+
+  classoptions.push(document.getElementById('c').options[document.getElementById('c').selectedIndex].text);
+
+  var selectedBackground = document.getElementById('e').value;
+  document.getElementById('f').value = backgroundarray[selectedBackground];
+  document.getElementById('randombackground').addEventListener('click', generaterandombackground);
+}
+
+showBackground();
 
 
+document.getElementById('randomize-all').addEventListener('click', generateAllclasses);
+function generateAllclasses() {
+  
 
 
+  // var selectedclass=document.getElementById('c').value;
+  // document.getElementById('d').value=classarray[selectedclass];
 
+  // classoptions.push(classarray[selectedclass]);
 
-
-
-
-
+  generaterandomrace();
+  generaterandomclass();
+  generaterandombackground();
+  generaterandomalignment();
+}
 
 
 
 //pick one button from 9 options;
 
-var n=0;
-var buttonel=document.querySelectorAll('.alel');
-for(var i=0;i<buttonel.length;i++){
-  buttonel[i].index=i;
-  buttonel[i].setAttribute('isAct','0');
-  buttonel[i].onclick=function(){
-    var isAct=this.getAttribute('isAct');
-    if(Number(isAct)){
-      this.setAttribute('isAct','0');
+var n = 0;
+var buttonel = document.querySelectorAll('.alel');
+for (var i = 0; i < buttonel.length; i++) {
+  buttonel[i].index = i;
+  buttonel[i].setAttribute('isAct', '0');
+  buttonel[i].onclick = function () {
+    var isAct = this.getAttribute('isAct');
+    if (Number(isAct)) {
+      this.setAttribute('isAct', '0');
     }
-    else{
+    else {
 
-      buttonel[n].setAttribute('isAct','0');
-      
-      this.setAttribute('isAct','1');
-      n=this.index;
+      buttonel[n].setAttribute('isAct', '0');
+
+      this.setAttribute('isAct', '1');
+      n = this.index;
     }
     alignmentoptions.push(buttonel[n].innerHTML);
   };
@@ -130,21 +175,21 @@ for(var i=0;i<buttonel.length;i++){
 
 
 //randomly pick one button
-document.getElementById('randomalignment').addEventListener('click',generaterandomalignment);
+document.getElementById('randomalignment').addEventListener('click', generaterandomalignment);
 
-function generaterandomalignment(e){
-  e.preventDefault();
+function generaterandomalignment() {
 
-  var buttonel=document.querySelectorAll('.alel');
 
-  var randomindexnumber=Math.floor(Math.random()*9);
-  for(var i=0;i<buttonel.length;i++){
+  var buttonel = document.querySelectorAll('.alel');
 
-    buttonel[i].setAttribute('isAct','0');
+  var randomindexnumber = Math.floor(Math.random() * 9);
+  for (var i = 0; i < buttonel.length; i++) {
+
+    buttonel[i].setAttribute('isAct', '0');
   }
 
-  n=randomindexnumber;
-  buttonel[randomindexnumber].setAttribute('isAct','1');
+  n = randomindexnumber;
+  buttonel[randomindexnumber].setAttribute('isAct', '1');
   alignmentoptions.push(buttonel[n].innerHTML);
 }
 
@@ -156,12 +201,12 @@ function generaterandomalignment(e){
 //try to use local storage to save data user pick
 
 
-function savedata(){
+function savedata() {
 
-  localStorage.setItem('race',JSON.stringify(raceoptions));
-  localStorage.setItem('class',JSON.stringify(classoptions));
-  localStorage.setItem('background',JSON.stringify(backgroundoptions));
-  localStorage.setItem('alignment',JSON.stringify(alignmentoptions));
+  localStorage.setItem('race', JSON.stringify(raceoptions));
+  localStorage.setItem('class', JSON.stringify(classoptions));
+  localStorage.setItem('background', JSON.stringify(backgroundoptions));
+  localStorage.setItem('alignment', JSON.stringify(alignmentoptions));
 }
 
 
@@ -170,25 +215,23 @@ function savedata(){
 
 //submit button
 
-document.getElementById('submitbutton').addEventListener('click',handlesubmit);
+document.getElementById('submitbutton').addEventListener('click', handlesubmit);
 
-function handlesubmit(e){
-
-  e.preventDefault();
+function handlesubmit(e) {
 
 
   // alert('You chose Race: ' + raceoptions[raceoptions.length-1] + ' \n You chose class: '+ classoptions[classoptions.length-1]);
 
-  var like=window.confirm('Your Race is : ' + raceoptions[raceoptions.length-1] +
-  ' \n Your Class is : '+ classoptions[classoptions.length-1]+
-  '\n Your Background is :' + backgroundoptions[backgroundoptions.length-1]+
-  '\n Your Alignment is :'+ alignmentoptions[alignmentoptions.length-1]+
-  '\nDo you like what we chose for you?');
-  if(like===true)
-  { 
+  var like = window.confirm('Your Race is : ' + raceoptions[raceoptions.length - 1] +
+    ' \n Your Class is : ' + classoptions[classoptions.length - 1] +
+    '\n Your Background is :' + backgroundoptions[backgroundoptions.length - 1] +
+    '\n Your Alignment is :' + alignmentoptions[alignmentoptions.length - 1] +
+    '\nDo you like what we chose for you?');
+  if (like === true) {
     savedata();
-    window.location.href='result.html';}
-  else{
+    window.location.href = 'result.html';
+  }
+  else {
 
     // document.write('ok no worries!');
     location.reload();
